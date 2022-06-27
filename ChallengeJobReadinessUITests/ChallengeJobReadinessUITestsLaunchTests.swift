20922cc84e1536b8ec684b8a3ce5861b7c56ffe5
Guilherme Wilhelm Magnabosco <guilherme.magnabosco@mercadolivre.com>
2022-06-27 16:47:40 -0300

    Initial Commit

---
+++ ChallengeJobReadinessUITests/ChallengeJobReadinessUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChallengeJobReadinessUITestsLaunchTests.swift
+//  ChallengeJobReadinessUITests
+//
+//  Created by Guilherme Wilhelm Magnabosco on 27/06/22.
+//
+
+import XCTest
+
+class ChallengeJobReadinessUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
